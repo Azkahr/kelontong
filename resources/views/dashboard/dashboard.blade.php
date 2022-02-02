50df829e0ddd1b7ea0b9da7770b9bf7bc0acda31
Azka <azkahar@gmail.com>
2022-02-02 14:52:15 +0700

    menambahkan tombol logout pada dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -133,14 +133,10 @@
                         </div>
                         <ul class="navbar-nav justify-content-end">
                             <li class="nav-item d-flex align-items-center">
-                                <a
-                                    href="javascript:;"
-                                    class="nav-link text-white font-weight-bold px-0"
-                                >
-                                    <i class="fa fa-user me-sm-1"></i>
-                                    <span class="d-sm-inline d-none"
-                                        >Sign In</span
-                                    >
+                                    <form action="/logout" method="post">
+                                    @csrf
+                                    <button type="submit" class="btn btn-primary border-0">Logout</button>
+                                    </form>
                                 </a>
                             </li>
                         </ul>
--- resources/views/dashboard/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main')
-
-@section('container')
-    <h1>Test</h1>
-        <form action="/logout" method="post">
-            @csrf
-            <button type="submit" class="btn btn-dark border-0">Logout</button>
-        </form>
-@endsection
