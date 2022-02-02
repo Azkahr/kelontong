bef7d19fd83a5f04286f45bf916e20445c29134e
rich0021 <kurabersayap002@gmail.com>
2022-02-02 16:38:09 +0700

    icon dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,17 +1,3 @@
-<!--
-=========================================================
-* Argon Dashboard 2 - v2.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://www.creative-tim.com/license)
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
--->
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -25,7 +11,9 @@
             sizes="76x76"
             href="assets/img/apple-icon.png"
         />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <script src="https://unpkg.com/feather-icons"></script>
+        <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
         <link rel="icon" type="image/png" href="assets/img/favicon.png" />
         <title>Kelontong | Dashboard</title>
         <!--     Fonts and icons     -->
@@ -74,10 +62,10 @@
                 id="sidenav-collapse-main"
             >
             
-            <!-- Navbar Content-->
+            <!-- Sidenav Content-->
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" href="pages/dashboard.html">
+                        <a class="nav-link {{ Request::is('dashboard') ? 'active' : '' }}" href="pages/dashboard.html">
                             <div
                                 class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center"
                             >
@@ -86,6 +74,26 @@
                                 ></i>
                             </div>
                             <span class="nav-link-text ms-1">Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{ Request::is('tambah-produk') ? 'active' : '' }}" href="pages/dashboard.html">
+                            <div
+                                class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center"
+                            >
+                                <img src="assets/img/plus.svg" alt="">
+                            </div>
+                            <span class="nav-link-text ms-1">Tambah Produk</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="pages/dashboard.html">
+                            <div
+                                class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center"
+                            >
+                                <img src="assets/img/log-out.svg" alt="">
+                            </div>
+                            <span class="nav-link-text ms-1">Logout</span>
                         </a>
                     </li>
                 </ul>
@@ -133,11 +141,17 @@
                         </div>
                         <ul class="navbar-nav justify-content-end">
                             <li class="nav-item d-flex align-items-center">
-                                    <form action="/logout" method="post">
-                                    @csrf
-                                    <button type="submit" class="btn btn-primary border-0">Logout</button>
-                                    </form>
-                                </a>
+                                <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                                    <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
+                                        <div style="transform:scale(1.4)">
+                                            <div class="sidenav-toggler-inner">
+                                                <i class="sidenav-toggler-line bg-white"></i>
+                                                <i class="sidenav-toggler-line bg-white"></i>
+                                                <i class="sidenav-toggler-line bg-white"></i>
+                                            </div>
+                                        </div sty>
+                                    </a>
+                                </li>
                             </li>
                         </ul>
                     </div>
@@ -147,12 +161,100 @@
 
             <!-- Content -->
             <div class="container-fluid py-4">
+                <div class="row d-flex justify-content-evenly">
+
+                    <!-- Info 1 -->
+                    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                    <div class="card">
+                        <div class="card-body p-3">
+                        <div class="row">
+                            <div class="col-8">
+                            <div class="numbers">
+                                <p class="text-sm mb-0 text-uppercase font-weight-bold">Jumlah Produk</p>
+                                <h5 class="font-weight-bolder">
+                                "jumlah produk"
+                                </h5>
+                            </div>
+                            </div>
+                            <div class="col-4 text-end">
+                            <div class="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle d-flex align-items-center justify-content-center">
+                                <img src="assets/img/box.svg" alt="">
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
+                    <!-- Info 2 -->
+                    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                    <div class="card">
+                        <div class="card-body p-3">
+                        <div class="row">
+                            <div class="col-8">
+                            <div class="numbers">
+                                <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Quantity</p>
+                                <h5 class="font-weight-bolder">
+                            total quantity
+                                </h5>
+                            </div>
+                            </div>
+                            <div class="col-4 text-end">
+                            <div class="icon icon-shape bg-gradient-danger shadow-danger text-center rounded-circle d-flex align-items-center justify-content-center">
+                                <img src="assets/img/layers.svg" alt="">
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
+                    <!-- Info 3 -->
+                    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                    <div class="card">
+                        <div class="card-body p-3">
+                        <div class="row">
+                            <div class="col-8">
+                            <div class="numbers">
+                                <p class="text-sm mb-0 text-uppercase font-weight-bold">Jenis Produk</p>
+                                <h5 class="font-weight-bolder">
+                                jenis produk
+                                </h5>
+                                <p class="mb-0">
+                            </div>
+                            </div>
+                            <div class="col-4 text-end">
+                            <div class="icon icon-shape bg-gradient-success shadow-success text-center rounded-circle d-flex align-items-center justify-content-center">
+                                <img src="assets/img/filter.svg" alt="">
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+
                 @yield('content')
             </div>
         </main>
         <!--   Core JS Files   -->
-        <script src="assets/js/core/bootstrap.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
+        <script src="assets/js/plugins/smooth-scrollbar.min.js"></script>
+        <script>
+            var win = navigator.platform.indexOf('Win') > -1;
+            if (win && document.querySelector('#sidenav-scrollbar')) {
+            var options = {
+                damping: '0.5'
+            }
+            Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+            }
+        </script>
         <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
         <script src="assets/js/argon-dashboard.min.js?v=2.0.0"></script>
+        <script>
+            feather.replace()
+        </script>           
     </body>
 </html>
