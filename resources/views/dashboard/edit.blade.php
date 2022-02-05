5d45c368697f18492d3a2d0594265547efc0c04b
Azka <azkahar@gmail.com>
2022-02-05 08:01:41 +0700

    menambahkan fitur preview pada halaman edit, tapi gambar masih belum tampil

--- resources/views/dashboard/edit.blade.php
+++ resources/views/dashboard/edit.blade.php
@@ -53,8 +53,13 @@
                 </div>
     
                 <div class="mb-3">
-                    <label for="image" class="form-label mt-4">Foto Produk</label>
-                    <input type="file" id="image" name="image" class="form-control @error('image') is-invalid @enderror" multiple>
+                    <label for="image" class="form-label">Foto Produk</label>
+                    @if ($product->image)
+                        <img src="{{ asset('storage/' . $product->image) }}" class="img-preview img-fluid mb-3 col-sm-5 d-block">
+                    @else
+                        <img class="img-preview img-fluid mb-3 col-sm-5">
+                    @endif
+                    <input type="file" id="image" name="image" class="form-control @error('image') is-invalid @enderror" multiple onchange="previewImage()">
                     @error('image')
                         <div class="invalid-feedback">
                             {{ $message }}
@@ -69,4 +74,19 @@
             
         </div>
     </div>
+    <script>
+        
+        function previewImage(){
+            const image = document.querySelector('#image');
+            const imgPreview = document.querySelector('.img-preview');
+            
+            const oFReader = new FileReader();
+            oFReader.readAsDataURL(image.files[0]);
+            
+            oFReader.onload = function(oFREvent){
+                imgPreview.src = oFREvent.target.result;
+            }
+        }
+        
+    </script>
 @endsection
