85a8367a4672cf090f4bba56fff7456f5d5642a5
Azka <azkahar@gmail.com>
2022-02-07 14:38:26 +0700

    mencoba membuat sistem role based login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -13,6 +13,14 @@ class LoginController extends Controller
         ]);
     }
 
+    protected function redirectTo(){
+        if(Auth()->user()->role == 1){
+            return route('dashboard');
+        } elseif(Auth()->user() == 2){
+            return redirect('/');
+        }
+    }
+    
     public function authenticate(Request $request){
         $credentials = $request->validate([
             "email" => "required|email:dns|exists:users,email",
@@ -22,13 +30,19 @@ class LoginController extends Controller
         ]);
 
         if(Auth::attempt($credentials)){
-            $request->session()->regenerate();
+            if(Auth::user()->role == 1){
+                return redirect()->intended('/dashboard')->with('success', 'Login successfully (refresh untuk menghilangkan notifikasi)');
+            } elseif(Auth::user()->role == 2){
+                return redirect()->route('home');
             
+            $request->session()->regenerate();
             return redirect()->intended('/dashboard')->with('success', 'Login successfully (refresh untuk menghilangkan notifikasi)');
+            
         } else {
             return back()->with('fail', 'Login failed');
         }
     }
+}
 
     public function logout(Request $request){
         Auth::logout();
