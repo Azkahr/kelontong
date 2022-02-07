f6e4ebfb20ba62a62ff7d4ae5554aca8bdc58795
Azka <azkahar@gmail.com>
2022-02-07 11:46:15 +0700

    memperbaiki notif dan menambahkan pengkondisian pada form image di halaman edit

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -24,7 +24,7 @@ class LoginController extends Controller
         if(Auth::attempt($credentials)){
             $request->session()->regenerate();
             
-            return redirect()->intended('/dashboard')->with('success', 'Login successfully');
+            return redirect()->intended('/dashboard')->with('success', 'Login successfully (refresh untuk menghilangkan notifikasi)');
         } else {
             return back()->with('fail', 'Login failed');
         }
