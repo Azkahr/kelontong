062b9d90047926ea6d02fe9c3fdead45abdd19a9
rich0021 <kurabersayap002@gmail.com>
2022-02-07 10:53:17 +0700

    fix live qty with latest()

--- app/Http/Livewire/Products.php
+++ app/Http/Livewire/Products.php
@@ -10,53 +10,38 @@ class Products extends Component
 {
     use WithPagination;
 
-    public $qty = [];
-    public $idU = [];
-    public $indicator = false;
+    public $products, $qty;
 
     public function mount(){
-        $this->qty = Product::pluck('qty');
-        $this->idU = Product::pluck('id');
+        $this->products = Product::latest()->get();
+        $this->qty = Product::latest()->pluck('qty');
     }
 
     public function render()
     {
-        return view('livewire.product', ['products' => Product::select('id','product_name')->where('users_id', auth()->user()->id)->paginate(10)]);
+        return view('livewire.product');
     }
 
     public function refresh()
     {
-    
+        $this->products = Product::latest()->get();
     }
 
-    public function plus($index){
+    public function plus(Product $product, $index){
+        $array = Product::where('id',$product->id)->pluck('qty')->toArray();
+        $increment = $array[0] + 1;
+        $array = Product::find($product->id)->update([
+            'qty' => $increment
+        ]);
         $this->qty[$index] = $this->qty[$index] + 1;
-        $this->indicator = true;
     }
 
-    public function min($index){
+    public function min(Product $product, $index){
+        $array = Product::where('id',$product->id)->pluck('qty')->toArray();
+        $decrement = $array[0] - 1;
+        $array = Product::find($product->id)->update([
+            'qty' => $decrement
+        ]);
         $this->qty[$index] = $this->qty[$index] - 1;
-        if($this->qty[$index] < 0){
-            $this->qty[$index] = 0;
-        }
-        $this->indicator = true;
-    }
-
-    public function remove(){
-        $this->qty = Product::pluck('qty');
-        $this->indicator = false;
-        $this->idU = [];
-    }
-
-    public function save(){
-        $uni = collect($this->idU)->toArray(); 
-        $val = collect($this->qty)->toArray();
-        $length = sizeof($val) - 1;
-        for($i = 0; $i <= $length; $i++){
-            Product::find($uni[$i])->update([
-                'qty' => $val[$i]
-            ]);
-        }
-        $this->indicator = [];
     }
 }
--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -1,40 +1,39 @@
 <div>
-    <div class="d-flex justify-content-center" style="margin-top:100px">
-        <table class="table-striped table-sm col-md-8 text-center">
-            <thead>
-                <tr>
-                    <th class="border px-4 py-2">No</th>
-                    <th class="border px-4 py-2">Nama Product</th>
-                    <th class="border px-4 py-2">Qty</th>
-                    <th class="border px-4 py-2">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($products as $product)
+    @if ($products->count())
+        <div class="d-flex justify-content-center" style="margin-top:100px">
+            <table class="table-striped table-sm col-md-8 text-center">
+                <thead>
                     <tr>
-                        <td class="border px-4 py-2">{{ $loop->iteration }}</td>
-                        <td class="border px-4 py-2">{{ $product->product_name }}</td>
-                        <td class="border px-4 py-2">
-                            <button style="border: none; background-color:transparent; font-size:22px" wire:click="plus({{ $loop->index }})">+</button>
-                            {{ $qty[$loop->index] }}
-                            <button style="border: none; background-color:transparent; font-size:24px" wire:click="min({{ $loop->index }})">-</button>
-                        </td>
-                        <td class="border px-4 py-2">
-                            <a href="/dashboard/update/{{ $product->id }}" class="badge bg-warning">Edit</a>
-                            <form action="/dashboard/delete/{{ $product->id }}" method="post" class="d-inline">
-                                @csrf
-                                <button class="badge bg-danger border-0" onclick="return confirm('Are you sure you want to delete this?')">Delete</button>
-                            </form>
-                        </td>
+                        <th class="border px-4 py-2">No</th>
+                        <th class="border px-4 py-2">Nama Product</th>
+                        <th class="border px-4 py-2">Qty</th>
+                        <th class="border px-4 py-2">Action</th>
                     </tr>
-                @endforeach
-                <tr>
-                    @if ($indicator)
-                        <th class="px-4 py-2" colspan="3"><div class="d-flex justify-content-end" style="margin-left: 20px"><button class="btn btn-success" wire:click="save">Simpan</button><button class="btn btn-danger" wire:click="remove" style="margin-left: 20px">Buang</button></div></th>
-                    @endif
-                </tr>
+                </thead>
+                <tbody>
+                    @foreach ($products as $product)
+                        <tr>
+                            <td class="border px-4 py-2">{{ $loop->iteration }}</td>
+                            <td class="border px-4 py-2">{{ $product->product_name }}</td>
+                            <td class="border px-4 py-2">
+                                <button style="border: none; background-color:transparent; font-size:22px" wire:click="plus({{$product}}, {{ $loop->index }})">+</button>
+                                {{ $qty[$loop->index] }}
+                                <button style="border: none; background-color:transparent; font-size:24px" wire:click="min({{$product}}, {{ $loop->index }})">-</button>
+                            </td>
+                            <td class="border px-4 py-2">
+                                <a href="/dashboard/update/{{ $product->id }}" class="badge bg-warning">Edit</a>
+                                <form action="/dashboard/delete/{{ $product->id }}" method="post" class="d-inline">
+                                    @csrf
+                                    <button class="badge bg-danger border-0" onclick="return confirm('Are you sure you want to delete this?')">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
                 
-            </tbody>
-        </table>
-    </div>
+            </table>
+        </div>
+    @else
+        <div class="d-flex justify-content-center" style="margin-top:100px; opacity:65%"><h2 class="">Belum Ada Produk Yang Diposting</h2></div>
+    @endif
 </div>
