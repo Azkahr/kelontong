10e32381a5624d015c58a45727c78a184c36ec85
Muhammad Naufal Muttaqin <kurabersayap002@gmail.com>
2022-02-07 22:20:53 +0700

    merapikan kode di livewire product

--- app/Http/Livewire/Products.php
+++ app/Http/Livewire/Products.php
@@ -31,7 +31,7 @@ class Products extends Component
     public function plus(Product $product, $index){
         $array = Product::where('id',$product->id)->pluck('qty')->toArray();
         $increment = $array[0] + 1;
-        $array = Product::find($product->id)->update([
+        Product::find($product->id)->update([
             'qty' => $increment
         ]);
         $this->qty[$index] = $this->qty[$index] + 1;
@@ -40,7 +40,7 @@ class Products extends Component
     public function min(Product $product, $index){
         $array = Product::where('id',$product->id)->pluck('qty')->toArray();
         $decrement = $array[0] - 1;
-        $array = Product::find($product->id)->update([
+        Product::find($product->id)->update([
             'qty' => $decrement
         ]);
         $this->qty[$index] = $this->qty[$index] - 1;
