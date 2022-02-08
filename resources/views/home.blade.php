e855b3bf513588b6b8c867c6bf7341e278f1d531
Azka <azkahar@gmail.com>
2022-02-08 10:06:36 +0700

    memperbaiki link di halaman home, agar link kembali ke dashboard hanya bisa diakses oleh seller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,16 +8,16 @@
         </div>
     @endif
     @auth
-    <h6>Welcome, {{ auth()->user()->name }}</h6>
-    <form action="/logout" method="post">@csrf<button type="submit" style="margin:0; padding:2px; border: none; background-color:transparent"><span style="font-family:'Open Sans', sans-serif; font-size:14px; font-weight:600; color:#707C95" class="nav-link-text ms-1" >Logout</span></button></form>
-    @can('isSeller')
-    <a href="/dashboard">Kembali ke dashboard</a>
-    @endcan
+        <h6>Welcome, {{ auth()->user()->name }}</h6>
+        <form action="/logout" method="post">@csrf<button type="submit" style="margin:0; padding:2px; border: none; background-color:transparent"><span style="font-family:'Open Sans', sans-serif; font-size:14px; font-weight:600; color:#707C95" class="nav-link-text ms-1" >Logout</span></button></form>
+    @if (auth()->user()->role == 'seller')
+        <a href="/dashboard">Kembali ke dashboard</a>
+    @endif
     @else
-    <a href="/login">Login</a>
-    <a href="/register">Register</a>
-    <br>
-    <br>
-    <a href="/daftar">Register seller</a>
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+        <br>
+        <br>
+        <a href="/daftar">Register seller</a>
     @endauth
 @endsection
