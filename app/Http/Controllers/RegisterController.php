1561bf668ab2a8cfa507d270a6a8d5bc4bdf2f71
Azka <azkahar@gmail.com>
2022-02-09 10:15:02 +0700

    registrasi redirect ke login

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
 class RegisterController extends Controller
@@ -54,8 +55,14 @@ class RegisterController extends Controller
 
         User::create($validatedData);
 
+        if(Auth::attempt($request->only('email', 'password'))){
+            notify()->success('Register Sukses', 'Berhasil');
+            return redirect('/dashboard')->with('success', 'Registration successfully');
+        } else {
+            notify()->error('Register Gagal', 'Gagal');
+            return redirect('/register')->with('error', 'Registration failed');
+        }
         notify()->success('Register Sukses', 'Berhasil');
 
-        return redirect('/login');
     }
 }
