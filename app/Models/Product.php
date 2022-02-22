f165d9b0b25a2bea6d2b9d64ff0f7abf083e59c1
rich0021 <kurabersayap002@gmail.com>
2022-02-22 16:42:27 +0700

    image slider

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -27,10 +27,10 @@ class Product extends Model
     }
 
     public function user(){
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'users_id');
     }
 
     public function category(){
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class, 'category_id');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,46 +1,51 @@
 @extends('layouts.main')
+@section('head')
+<link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
+@endsection
 @section('container')
 @include('layouts/navbar')
-<div v-show="modal" class="cartPage">
-    <div class="cart">
-        
-    </div>
-</div>
-<div class="latest">
-    
-</div>
-<div id="category" class="category">
-
-</div>
-
-<div class="container mt-5" style="padding: 100px">
-    <div class="row">
-        @foreach ($products as $product)
-        <div class="card" style="width: 18rem; margin-right: 20px; margin-top: 5px">
-            <img src="{{ asset('storage/' . $product->image) }}" class="card-img-top" alt="{{ $product->product_name }}">
-            <div class="card-body">
-                <h5 class="card-title">{{ $product->product_name }}</h5>
-                <p class="card-text text-muted">{{ $product->category->name }}</p>
-                <p class="card-text">{!! $product->desc !!}</p>
-                <a href="#" class="btn btn-primary">Detail product</a>
+<div style="width: 100%; display:flex; justify-content:center; margin-top:200px">
+    @foreach ($products as $product)
+        <div class="kartu" style="width: 14rem; margin:0px 5px 0px 5px; border:1px solid #DFDFDF; border-radius:7px">
+            <div class="swiper">
+                <div class="swiper-wrapper">
+                    @foreach (explode(',',$product->image) as $item)
+                        <div class="swiper-slide" style="user-select: none"><img style="object-fit: cover; width: 100%; height:200px; border-top-right-radius:7px; border-top-left-radius:7px" src="{{ asset('storage/'.$item) }}" alt="{{ $product->name }}"></div>
+                    @endforeach
+                </div>
+                <div class="swiper-buttonP">
+                    <span style="margin:3px" data-feather="arrow-left"></span>
+                </div>
+                <div class="swiper-buttonN">
+                    <span style="margin:3px" data-feather="arrow-right"></span>
+                </div>
+            </div>
+            <div class="content">
+                <h6 style="font-size:18px">{{ $product->product_name }}</h6>
+                <p style="margin:0; font-weight:bold">RP {{ number_format($product->harga, 0,",",".") }}</p>
+                <p style="margin:0">{{ $product->user->nama_toko}}</p>
+                <a style="margin:20px 0px 25px 0px" href="#" class="btn btn-primary">Lihat Produk</a>
             </div>
         </div>
-        @endforeach
-    </div>
+    @endforeach
 </div>
-<x:notify-messages />
+
 <script>
-    Vue.createApp({
-        data() {
-            return {
-                modal: false,
-            }
-        },
-        methods: {
-            showCart(){
-                this.modal = !this.modal
-            }
-        }
-    }).mount('#container')
+    feather.replace()
+</script>
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+<script>
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 'auto',
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-buttonN',
+    prevEl: '.swiper-buttonP',
+  },
+});
 </script>
 @endsection
