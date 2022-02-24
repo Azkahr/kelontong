2416412e2d56b46365360b1ded6fae2cd5d28d14
Azka <azkahar@gmail.com>
2022-02-24 12:53:44 +0700

    coba styling halaman search dan menghapus link di dropdown logout

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -22,7 +22,7 @@
                         <a href="/profile/update/{{ auth()->user()->id }}">Setting</a>
                         <form action="/logout" method="post">
                             @csrf
-                            <a href="#"><button id="logout" type="submit">Logout</button></a>
+                            <button style="margin-left: 16px; margin-bottom: 15px; margin-top: 10px;" id="logout" type="submit">Logout</button>
                         </form>
                     </div>
                 </div>
