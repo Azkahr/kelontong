645190c44639ac6db241d20e481e09241c5ec40f
Muhammad Naufal Muttaqin <kurabersayap002@gmail.com>
2022-03-21 21:18:49 +0700

    auth dengan google jadi

--- app/Http/Controllers/OAuthController.php
+++ app/Http/Controllers/OAuthController.php
@@ -16,7 +16,23 @@ class OAuthController extends Controller
 
     public function googleCallback()
     {
-       $user = Socialite::driver('google')->user();
-       dd($user);
-    }
+        $user_google = Socialite::driver('google')->user();
+        $user = User::where('email', $user_google->getEmail())->where('google_id', $user_google->getId())->first();
+
+        if($user != null){
+            auth()->login($user, true);
+            return redirect()->route('home');
+        }else{
+            $create = User::Create([
+                'email' => $user_google->getEmail(),
+                'name' => $user_google->getName(),
+                'role' => 'user',
+                'google_id' => $user_google->getId(),
+                'password' => 0,
+                'email_verified_at' => now()
+            ]);
+            auth()->login($create, true);
+            return redirect()->route('home');
+        }
+    } 
 }
