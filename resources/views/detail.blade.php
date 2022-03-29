8bfc561e1f824e47f82d0f481a2e4e0f595b9c6a
rich0021 <kurabersayap002@gmail.com>
2022-03-29 09:35:28 +0700

    menambah margin di hr

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -67,7 +67,7 @@
         </div>
         <input id="catatan" class="form-control mt-2" type="text" placeholder="Catatan Untuk Penjual">
         <hr class="mt-3 opacity-100 bg-light mx-auto" style="width: 90%">
-        <button id="addToCartBtn" class="btn w-100 mt-2" style="background-color:white; color:#536AEC; font-size:15px; font-family:'Spartan'; font-weight:600">Keranjang+</button>
+        <button id="addToCartBtn" class="btn w-100 mt-3" style="background-color:white; color:#536AEC; font-size:15px; font-family:'Spartan'; font-weight:600">Keranjang+</button>
         <button class="btn w-100 mt-2" style="border: 2px solid white; color:white; font-family:'Spartan'; font-weight:600">Beli</button>
     </div>
     <div class="product-comment">
