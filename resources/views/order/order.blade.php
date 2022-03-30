091b886633f167e1fcdf8c920494638f2f4d8067
rich0021 <kurabersayap002@gmail.com>
2022-03-30 12:22:45 +0700

    membuat layout table transaksi (not done)

--- resources/views/order/order.blade.php
+++ resources/views/order/order.blade.php
@@ -25,7 +25,7 @@
                     <textarea class="form-control" name="user_review" id="textArea" rows="5" placeholder="Tulis ulasan..." required></textarea>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn-close2 btn btn-secondary" data-bs-dismiss="modal">Close</button>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
             </form>
@@ -40,54 +40,54 @@
                     <h4 style="font-size: 2em; font-weight: bold">My Orders</h4>
                 </div>
                 <div class="card-body" id="transaksi-container">
-                    <table>
+                    <table class="table table-borderless">
                         @foreach ($orders as $order)
-                            <td>
-                                Tanggal Order : {{ date('d-m-Y', strtotime($order->created_at)) }}
-                            </td>
-                            <td>
-                                Nomor Resi : {{ $order->no_resi }}
-                            </td>
-                            <td>
-                                <a href="{{ url('view-order', $order->id) }}" class="btn btn-primary">View</a>
-                            </td>
                             <tr>
-                                <table class="table table-bordered">
-                                    <thead>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>
+                                    <div>
+                                        <p>Tanggal Order : {{ date('d-m-Y', strtotime($order->created_at)) }}</p>
+                                        <p>Nomor Resi : {{ $order->no_resi }}</p>
+                                        <a href="{{ url('view-order', $order->id) }}" class="btn btn-primary">View</a>
+                                    </div>
+                                    <table class="table table-bordered">
                                         <tr>
                                             <th>Nama Barang</th>
                                             <th>Kategori</th>
                                             <th>Harga</th>
                                             <th>Status</th>
+                                            @if ($order->status == "beres")
+                                                <th>Action</th>
+                                            @endif
+                                            
                                         </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($order->orderItems as $orderI)
-                                            <tr>
-                                                <td>{{ $orderI->products->product_name }}</td>
-                                                <td>{{ $orderI->products->category->name }}</td>
-                                                <td>Rp.{{ number_format($orderI->products->harga, 0,",",".") }}</td>
-                                                @if ($orderI->order->status == "kirim")
-                                                    <td>Sedang dikirim</td>
-                                                @elseif($orderI->order->status == "tolak")
-                                                    <td style="color: red; font-weight: bolder">Ditolak lihat alasannya di detail order</td>
-                                                @else
-                                                    <td>{{ $orderI->order->status }}</td>
-                                                @endif
-                                                @if ($orderI->order->status == "beres")
-                                                    <td>
-                                                        <input type="hidden" value="{{ $orderI->id }}" id="orderitemV">
-                                                        <button type="button" value="{{ $orderI->products->id }}" class="ratingBtn btn btn-primary" data-name="{{ $orderI->products->product_name }}" @if ($orderI->rating == null) data-rating=null data-review=null @else data-rating="{{$orderI->rating->stars_rated}}" data-review="{{ $orderI->rating->user_review }}" @endif data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                                            Beri Rating
-                                                        </button>
-                                                    </td>
-                                                @endif
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
+                                        <tr>
+                                            @foreach ($order->orderItems as $orderI)
+                                                <tr>
+                                                    <td>{{ $orderI->products->product_name }}</td>
+                                                    <td>{{ $orderI->products->category->name }}</td>
+                                                    <td>Rp.{{ number_format($orderI->products->harga, 0,",",".") }}</td>
+                                                    @if ($orderI->order->status == "kirim")
+                                                        <td>Sedang dikirim</td>
+                                                    @elseif($orderI->order->status == "tolak")
+                                                        <td style="color: red; font-weight: bolder">Ditolak lihat alasannya di detail order</td>
+                                                    @else
+                                                        <td>{{ $orderI->order->status }}</td>
+                                                    @endif
+                                                    @if ($orderI->order->status == "beres")
+                                                        <td>
+                                                            <input type="hidden" value="{{ $orderI->id }}" id="orderitemV">
+                                                            <button type="button" value="{{ $orderI->products->id }}" class="ratingBtn btn btn-primary" data-name="{{ $orderI->products->product_name }}" @if ($orderI->rating == null) data-rating=null data-review=null @else data-rating="{{$orderI->rating->stars_rated}}" data-review="{{ $orderI->rating->user_review }}" @endif data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                                                Beri Rating
+                                                            </button>
+                                                        </td>
+                                                    @endif
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </td>
                             </tr>
-                            <hr class="my-3">
                         @endforeach
                     </table>
                 </div>
@@ -155,7 +155,14 @@ $(function () {
         e.preventDefault();
         setTimeout(() => {
             $('#star-icon').empty();
-        }, 100);
+        }, 200);
+    });
+
+    $('.btn-close2').click(function (e) { 
+        e.preventDefault();
+        setTimeout(() => {
+            $('#star-icon').empty();
+        }, 200);
     });
 });
 </script>
